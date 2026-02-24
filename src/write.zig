9b1ade1f45e55654d3330675d9369367b005c407
arod1213 <aidan@aidanrodriguez.com>
2026-02-24 10:24:26 -0800

    fix nested struct

--- src/write.zig
+++ src/write.zig
@@ -8,6 +8,29 @@ const types = @import("./types.zig");
 const utils = @import("utils.zig");
 const Node = types.Node;
 
+const ValueType = union(enum) { field: []const u8, child: Node };
+fn getValue(alloc: Allocator, comptime T: type, value: T, field_name: []const u8, parent_node: Node) !ValueType {
+    const field_info = @typeInfo(T);
+    const value_str = switch (field_info) {
+        .null => "",
+        .optional => |opt| if (value == null) "" else try getValue(opt.child, alloc, field_name, value),
+        .bool => try std.fmt.allocPrint(alloc, "{any}", .{value}),
+        .int,
+        .comptime_int,
+        .float,
+        .comptime_float,
+        => try std.fmt.allocPrint(alloc, "{d}", .{value}),
+        .pointer => |ptr| if (ptr.child == u8) value else unreachable, // only support []const u8
+        .@"struct" => {
+            const child_node = try structToNode(T, alloc, value);
+            child_node.attach(&parent_node);
+            return .{ .child = child_node };
+        },
+        else => unreachable,
+    };
+    return .{ .field = value_str };
+}
+
 pub fn structToNode(comptime T: type, alloc: Allocator, x: T) !Node {
     const info = @typeInfo(T);
     assert(info == .@"struct");
@@ -16,19 +39,11 @@ pub fn structToNode(comptime T: type, alloc: Allocator, x: T) !Node {
     var parent: Node = try Node.new(struct_name, null);
 
     inline for (info.@"struct".fields) |field| {
-        const field_info = @typeInfo(field.type);
-        const val_str = switch (field_info) {
-            .bool => try std.fmt.allocPrint(alloc, "{any}", .{@field(x, field.name)}),
-            .int, .float => try std.fmt.allocPrint(alloc, "{d}", .{@field(x, field.name)}),
-            .pointer => |ptr| if (ptr.child == u8) @field(x, field.name) else unreachable, // only support []const u8
-            .@"struct" => {
-                const child_node = try structToNode(field.type, alloc, @field(x, field.name));
-                child_node.attach(&parent);
-                continue;
-            },
-            else => unreachable,
-        };
-        parent.setProperty(field.name, val_str);
+        const value = try getValue(alloc, field.type, @field(x, field.name), field.name, parent);
+        switch (value) {
+            .field => |f| parent.setProperty(field.name, f),
+            .child => {},
+        }
     }
     return parent;
 }
